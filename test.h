3a59eaf771367b6f9b71cdcda103d7e7ea8a67d7
Ole <itstheimp@gmail.com>
2013-09-23 00:54:19 +0200

    iso here we come

--- test.h
+++ test.h
@@ -3,15 +3,15 @@
 
 #include <features.h>
 
-#define COLOR_BLACK 		"\e[0;30m"
-#define COLOR_RED 			"\e[0;31m"
-#define COLOR_GREEN 		"\e[0;32m"
-#define COLOR_YELLOW 		"\e[0;33m"
-#define COLOR_BLUE 			"\e[0;34m"
-#define COLOR_PINK 			"\e[0;35m"
-#define COLOR_CYAN 			"\e[0;36m"
-#define COLOR_WHITE 		"\e[0;37m"
-#define COLOR_CLEAR 		"\e[0m"
+#define COLOR_BLACK 		"\033[0;30m"
+#define COLOR_RED 			"\033[0;31m"
+#define COLOR_GREEN 		"\033[0;32m"
+#define COLOR_YELLOW 		"\033[0;33m"
+#define COLOR_BLUE 			"\033[0;34m"
+#define COLOR_PINK 			"\033[0;35m"
+#define COLOR_CYAN 			"\033[0;36m"
+#define COLOR_WHITE 		"\033[0;37m"
+#define COLOR_CLEAR 		"\033[0m"
 
 typedef struct {
 	enum {
